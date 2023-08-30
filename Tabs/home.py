c09bc17da7dd885adb1464ddd433efbc26d33957
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-08-30 15:20:28 +0530

    Update home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -7,7 +7,7 @@ def app():
     """This function create the home page"""
     
     # Add title to the home page
-    st.title("Crop Yield Prediction System")
+    st.title("Crop Quantity Prediction System")
 
     # Add image to the home page
     st.image("./images/home.png")
